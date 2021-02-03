96a9f8cd83dab16d6674325f6ffa11364650ddab
Igor Voloshanenko <bob@example.com>
2021-02-03 14:27:20 -0500

    initial commit

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx:alpine
-
-EXPOSE 80
-
-COPY release /usr/share/nginx/html
-WORKDIR /usr/share/nginx/html
--- build_prod.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-source env.prod
-docker build -t click-2-dial .
-docker tag click-2-dial ${DOCKER_REGISTRY}/click-2-dial:latest
-docker push ${DOCKER_REGISTRY}/click-2-dial:latest
-python deploy/deploy.py
